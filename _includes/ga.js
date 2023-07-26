7447ed07b3ceeb659eccf8361c9cc1f97fb6a002
ekici <burakekici0@gmail.com>
2023-07-26 22:13:38 +0300

    GA is enabled back.

--- _includes/ga.js
+++ _includes/ga.js
@@ -1,6 +1,6 @@
 /*This function will load script and call the callback once the script has loaded*/
 
-function loadScriptAsync(scriptSrc, callback) {
+/*function loadScriptAsync(scriptSrc, callback) {
     if (typeof callback !== 'function') {
         throw new Error('Not a valid callback for async script load');
     }
@@ -8,15 +8,15 @@ function loadScriptAsync(scriptSrc, callback) {
     script.onload = callback;
     script.src = scriptSrc;
     document.head.appendChild(script);
-}
+}*/
 
 /* This is the part where you call the above defined function and "calls back" your code which gets executed after the script has loaded */
 
-loadScriptAsync('https://www.googletagmanager.com/gtag/js?id=UA-52187271-1', function () {
+/*loadScriptAsync('https://www.googletagmanager.com/gtag/js?id=UA-52187271-1', function () {
     window.dataLayer = window.dataLayer || [];
     function gtag() { dataLayer.push(arguments); }
     gtag('js', new Date());
     gtag('config', 'UA-52187271-1', { 'anonymize_ip': true });
-})
+})*/
 
 /* Bu dosyayı Privacy GDPR zımbırtısı için yarattık. Zaten GA dosyası vardı? */
